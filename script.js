000a0c2b6e0767198db2c0fe81d76ec3e0ca6e63
Frank Edina <frankedus@gmail.com>
2020-10-03 02:20:05 +0200

    Fixed the working of character counter.

--- script.js
+++ script.js
@@ -2,14 +2,27 @@ var body = $('body');
 
 $(document).ready(function() {
     dropdownActions();
+    countCharacters();
 });
 
 function dropdownActions() {
     body.off('mouseenter.show_dropdown').on('mouseenter.show_dropdown', '.dropdown_holder', function() {
-        $(this).find('.menu_dropdown').fadeIn();
+        $(this).find('.menu_dropdown').fadeIn(200);
     });
 
     body.off('mouseleave.hide_dropdown').on('mouseleave.hide_dropdown', '.dropdown_holder', function() {
-        $(this).find('.menu_dropdown').fadeOut();
+        $(this).find('.menu_dropdown').fadeOut(200);
+    });
+}
+
+
+function countCharacters () {
+
+    let maxCharacter = 200;
+
+    $('.character_textarea').on('keyup', function() {
+        let numberOfCharacters = $('.character_textarea').val().length;
+        $('.remain_character').html(maxCharacter - numberOfCharacters);
+
     });
 }
